838b5fb95dc38b4d2853fb9ec9a83db181706bc5
luizhenrique0512 <araujo.carvalho.luiz@escola.pr.gov.br>
2024-10-23 10:39:17 -0300

    Update estatistica.js

--- estatistica.js
+++ estatistica.js
@@ -1,22 +1,22 @@
-async function quantidadeVoto() {
-    const url = 'https://raw.githubusercontent.com/Akiovictor/trabalho_json_grafico/refs/heads/main/disciplina.json'
+const url = 'https://raw.githubusercontent.com/luizhenrique0512/trabalho_json_grafico/refs/heads/main/estatistica.json'
+
+async function vizualizarInformacoes() {
     const res = await fetch(url)
     const dados = await res.json()
-    const disciplina = Object.keys(dados)
-    const votos = Object.values(dados)
+    const Brasil = (dados.Brasil)/1000000
+    const norte = (dados.norte)/1000000
+    const nordeste = (dados.nordeste)/1000000
+    const centrooeste = (dados.centrooeste)/1000000
+    const sul = (dados.sul)/1000000
+    const suldeste = (dados.suldeste)/1000000
+    const Brasil2010 = (dados.Brasil2010)/1000000
 
-    const data = [
-        {
-            x: disciplina, 
-            y: votos, 
-            type: 'bar'
-        }
-    ]
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `Sabia que no Brasil temos <span>${Brasil} milhões</span> de habitantes e deles temos <span> ${norte} mi</span> no norte,nordeste <span> ${nordeste} mi </span>, suldeste <span> ${suldeste} Mi</span>, no sul ${sul} e <span>  ${centrooeste} mi </span> no centro-oeste. Em 2010 o Brasil tinha <span>  ${Brasil2010} </span> milhões, o que quer dizer que o Brasil cresceu <span> 11,5% </span> populacionamente`
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-quantidadeVoto()
+vizualizarInformacoes() 
